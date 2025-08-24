4d654644d8f43e6e45ab1a6c47bd0f4cd176e3d7
Siddharth Thakur <u803760@gmail.com>
2025-08-24 12:24:06 +0530

    added domain

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/tasks', taskRoutes)
+app.use('/api/tasks', taskRoutes)
 
 app.get('/', (req,res) => {
     res.send("Task manager api is working")
